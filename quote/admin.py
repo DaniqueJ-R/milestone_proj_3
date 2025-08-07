08be767e88c3baa3cd8ddf83a5fa660fd34de15c
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-08-07 18:48:26 +0100

    Added notes model and admin access

--- quote/admin.py
+++ quote/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
+from .models import Notes, 
 
 # Register your models here.
 
-
-# from django.contrib import admin
-# from .models import Post, Comment
-
-# # Register your models here.
-# admin.site.register(Post)
-# admin.site.register(Comment)
+admin.site.register(Notes)
--- quote/models.py
+++ quote/models.py
@@ -2,3 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+    CATEGORY = (
+        (0, "Uncategorized"),
+        (1, "Stress"),
+        (2, "Shame"),
+        (3, "Grief"),
+        (4, "Self-worth"),
+    )
+
+class Notes(models.Model):
+    content = models.TextField()
+    name = models.CharField(max_length=100, default='anonymous')
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    category = models.IntegerField(choices=CATEGORY, default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+
+    # def __str__(self):
+    #     return self.name
