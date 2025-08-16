c1df624a49481e30358cac3f06b83a8205914bb2
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-08-16 14:28:19 +0100

    Removed apps that will not be in use

--- report/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def intro(request):
-    return HttpResponse("Hi HI. You're now on the reports app.")
